321d6754e6745b931fe845eb23757c93e13494e0
mnidchenko <maria.nidchenko@dxc.com>
2022-10-24 12:05:26 +0300

    Complete 3

---
+++ Inheritance/src/hero/Hero.java
@@ -0,0 +1,35 @@
+package hero;
+
+public class Hero {
+    String username;
+    int level;
+
+    public Hero(String username, int level) {
+        this.username = username;
+        this.level = level;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Type: %s Username: %s Level: %s",
+                this.getClass().getName(),
+                this.getUsername(),
+                this.getLevel());
+    }
+}
