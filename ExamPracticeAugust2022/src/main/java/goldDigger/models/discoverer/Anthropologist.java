9196a88e0f1e76343627eaeeecc3b103f124c9d6
mnidchenko <maria.nidchenko@dxc.com>
2022-11-04 14:43:29 +0200

    testing uploaded

--- ExamPracticeAugust2022/src/main/java/goldDigger/models/discoverer/Anthropologist.java
+++
@@ -1,9 +0,0 @@
-package goldDigger.models.discoverer;
-
-public class Anthropologist extends BaseDiscoverer{
-    private static final double ANTHROPOLOGIST_INITIAL_ENERGY = 40;
-
-    public Anthropologist(String name) {
-        super(name, ANTHROPOLOGIST_INITIAL_ENERGY);
-    }
-}
--- ExamPracticeAugust2022/src/main/java/goldDigger/models/discoverer/Archaeologist.java
+++
@@ -1,9 +0,0 @@
-package goldDigger.models.discoverer;
-
-public class Archaeologist extends BaseDiscoverer {
-    private final static double ARCHEOLOGIST_INITIAL_ENERGY = 60;
-    public Archaeologist(String name) {
-        super(name, ARCHEOLOGIST_INITIAL_ENERGY);
-    }
-
-}
